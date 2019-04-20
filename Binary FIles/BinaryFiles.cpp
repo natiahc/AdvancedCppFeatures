976955015b15ed6ba02d01e44bd3839d228b169a
Chaitan Divagi <natiahc@gmail.com>
2019-04-20 23:55:47 +0530

    read and write Binary Files;
    Signed-off-by: Chaitan Divagi <natiahc@gmail.com>

--- Binary FIles/BinaryFiles.cpp
+++ Binary FIles/BinaryFiles.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 #pragma pack(push, 1)
 
@@ -7,13 +8,54 @@ struct Person
 {
     char name[50];
     int age;
-    double weight;
+    double height;
 };
 
 #pragma pack(pop)
 
 int main()
 {
-    std::cout << sizeof(Person) << std::endl; 
+    Person someone = {"Chaitan", 30, 1.8};
+
+    std::string fileName = "test.bin";
+
+    //// Write Binary file /////////////
+
+    //std::ofstream outputFile;
+    //outputFile.open(fileName, std::ios::binary);
+
+    std::fstream outputFile;
+    outputFile.open(fileName, std::ios::binary | std::ios::out);
+
+    if (outputFile.is_open())
+    {
+        //outputFile.write((char *)&someone, sizeof(Person));
+        outputFile.write(reinterpret_cast<char *>(&someone), sizeof(Person));
+        outputFile.close();    
+    }
+    else 
+    {
+        std::cout << "could not create file " + fileName;
+    }
+
+    //// Read Binary file /////////////
+
+    Person someoneElse = {};
+
+    std::ifstream inputFile;
+    inputFile.open(fileName, std::ios::binary);
+
+    if (inputFile.is_open())
+    {
+        inputFile.read(reinterpret_cast<char *>(&someoneElse), sizeof(Person));
+        inputFile.close();    
+    }
+    else 
+    {
+        std::cout << "could not read file " + fileName;
+    }
+    
+    std::cout << someoneElse.name << ", " << someoneElse.age << ", " << someoneElse.height << std::endl;
+
     return 0;
 }
