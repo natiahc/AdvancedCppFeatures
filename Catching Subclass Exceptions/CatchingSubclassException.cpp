5cab43fcc8b474eada256c26e776ecb44ee99a4e
Chaitan Divagi <natiahc@gmail.com>
2019-04-20 16:23:15 +0530

    Catching Subclass Exceptions;
    Signed-off-by: Chaitan Divagi <natiahc@gmail.com>

---
+++ Catching Subclass Exceptions/CatchingSubclassException.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <exception>
+
+void goesWrong()
+{
+    bool error1Detected = true;
+    bool error2Detected = false;
+
+    if(error1Detected)
+    {
+        throw std::bad_alloc();
+    }
+
+    if(error2Detected)
+    {
+        throw std::exception();
+    }
+}
+
+int main()
+{
+    try 
+    {
+        goesWrong();
+    }
+    catch (std::bad_alloc &e)
+    {
+        std::cout << "Catching bad_alloc: " << e.what() << std::endl;
+    }
+    catch(std::exception &e)
+    {
+        std::cout << "Catching exception: " << e.what() << std::endl;
+    }
+
+    return  0;
+}
