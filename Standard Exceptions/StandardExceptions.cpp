cdf33d4f3cedc4203c268bc71bddc3510f0501f0
Chaitan Divagi <natiahc@gmail.com>
2019-04-20 15:42:44 +0530

    Standard Exceptions;
    Signed-off-by: Chaitan Divagi <natiahc@gmail.com>

---
+++ Standard Exceptions/StandardExceptions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class CanGoWrong
+{
+
+public:
+    CanGoWrong()
+    {
+        char *pMemory = new char[999999999999999999];
+        delete[] pMemory;
+    }
+
+};
+
+int main()
+{
+    try 
+    {
+        CanGoWrong wrong;
+    }
+    catch(std::bad_alloc &e)
+    {
+        std::cout << "Caught exception: " << e.what() << std::endl;
+    }
+
+    std::cout << "Still running" << std::endl;
+}
