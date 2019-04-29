51b5683e743f4b5999a48c48e349627c9dfdf1f8
Chaitan Divagi <natiahc@gmail.com>
2019-04-29 22:21:24 +0530

    Overloading Comparison Operator;
    Signed-off-by: Chaitan Divagi <natiahc@gmail.com>

---
+++ Overloading Comparison Operator/OverloadingComparisonOperator.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+
+class Complex 
+{
+
+private:
+    double real;
+    double imaginary;
+
+public:
+    Complex() : real(0), imaginary(0)
+    {
+
+    }
+
+    Complex(double real, double imaginary) : real(real), imaginary(imaginary)
+    {
+
+    }
+
+    Complex(const Complex &other)
+    {
+        real = other.real;
+        imaginary = other.imaginary;
+    }
+
+    const Complex &operator=(const Complex &other)
+    {
+        real = other.real;
+        imaginary = other.imaginary;
+
+        return *this;
+    }
+
+    double getReal() const
+    {
+        return real;
+    }
+
+    double getImaginary() const
+    {
+        return imaginary;
+    }
+
+    bool operator==(const Complex &other) const
+    {
+        return (real == other.real) && (imaginary == other.imaginary);
+    }
+
+    bool operator!=(const Complex &other) const
+    {
+        return !(*this==other);
+    }
+};
+
+
+
+ std::ostream &operator<<(std::ostream &out, Complex &c)
+{
+    out << "(" << c.getReal() << ", " << c.getImaginary() << ")";
+    return out;
+}
+
+Complex operator+(const Complex &c1, const Complex &c2)
+{
+    return Complex(c1.getReal()+c2.getReal(), c1.getImaginary()+c2.getImaginary());
+}
+
+Complex operator+(const Complex &c1, double d)
+{
+    return Complex(c1.getReal()+d, c1.getImaginary()+d);
+}
+
+Complex operator+(double d, const Complex &c2)
+{
+    return Complex(d+c2.getReal(), d+c2.getImaginary());
+}
+
+int main()
+{
+    Complex c1(3, 4);
+    Complex c2(2, 3);
+
+    if (c1 == c2)
+    {
+        std::cout << "Equal" << std::endl;
+    }
+    else
+    {
+        std::cout << "Not equal" << std::endl;
+    }
+
+    if (c1 != c2)
+    {
+        std::cout << "Not equal" << std::endl;
+    }
+    else
+    {
+        std::cout << "Equal" << std::endl;
+    }
+    
+    return 0;
+}
