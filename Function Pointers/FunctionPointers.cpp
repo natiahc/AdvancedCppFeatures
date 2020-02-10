4c115a3beae054501e5686d593028db7868b80e0
Chaitan Divagi <natiahc@gmail.com>
2020-02-10 19:51:37 +0530

    function pointer 2;
    Signed-off-by: Chaitan Divagi <natiahc@gmail.com>

--- Function Pointers/FunctionPointers.cpp
+++ Function Pointers/FunctionPointers.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
+#include <cmath>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int prod(int a, int b)
+{
+    return a * b;
+}
+
+int CallFunc(int(*pFunc)(int, int), int a, int b)
+{
+    return pFunc(a, b);
+}
 
 void test()
 {
@@ -10,18 +26,37 @@ void test1(int value)
     std::cout << "value: " << value << std::endl;
 }
 
+double CallDblFunc(double(*pFunc)(double), double arg)
+{
+    return pFunc(arg);
+}
+
 int main()
 {
     test();
     //void (*pTest)(); // a variable pTest which is a pointer to a function with a void return type and no parameters
 
-    void (*pTest)(int) = test1; // we could initialize this variable
+    //void (*pTest)(int) = test1; // we could initialize this variable
 
    // pTest = &test; // or pTest = test; also works!!
 
     //(*pTest)(); // pr pTest(); 
 
-    pTest(5);
+    //pTest(5);
+
+    std::cout << CallFunc(sum, 1, 2) << std::endl;
+    std::cout << CallFunc(prod, 3, 2) << std::endl;
+
+    double pi = atan(1.0)*4.0;
+    std::cout << pi << std::endl;
+
+    std::cout << CallDblFunc(sin, pi/2.0) << std::endl;
+    std::cout << CallDblFunc(cos, pi/2.0) << std::endl;
+
+    int intg;
+    typedef int(*IntFuncPtr) (int, int);
+    IntFuncPtr fpt = sum;
+    std::cout << fpt(3, 4);
 
     return 0;
 }
